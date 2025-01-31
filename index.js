f51ea6e598fcae2d1169938e45456595b631ebfa
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-01-31 14:39:47 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let numero1 = document.getElementById('numero1');
 let numero2 = document.getElementById('numero2');
 let botao = document.getElementById('calcular');
 let res = document.getElementById('resultado');
-
+asdadasd
 function calcular(n1, n2) {
 ssDSASDAS
     return n1 + n2;
